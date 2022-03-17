fcc379d5f1dca0f8fe5add3a1ee2664b13363a55
abraaovilanova <abraaovilanova@gmail.com>
2022-03-17 13:24:10 -0300

    error allowedHeaders

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const app = express()
 
 app.use(cors({
-    origin: '*'
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json())
